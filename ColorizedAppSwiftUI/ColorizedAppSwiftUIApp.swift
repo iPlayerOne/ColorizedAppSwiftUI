ba866437b92eb88156921e7acdfba59c80c29e79
Ivan Korobov <ivan.koroboff@gmail.com>
2022-09-12 07:49:10 +0300

    Initial Commit

---
+++ ColorizedAppSwiftUI/ColorizedAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwiftUIApp.swift
+//  ColorizedAppSwiftUI
+//
+//  Created by ikorobov on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
